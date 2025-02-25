f08648e97a244a85bb20e20241a537a8fa4d66c0
pateldaksh55 <patel.daksh.7792@gmail.com>
2025-02-25 16:27:37 +0530

    Assignment_2203031080009

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+import { UserComponent } from './auth-user/user/user.component';
+import { ForgotPasswordComponent } from './auth-user/forgot-password/forgot-password.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent,RegisterComponent,UserComponent,ForgotPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
