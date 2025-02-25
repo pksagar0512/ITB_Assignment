747920e1b5447043f1064a9d2a8744efc7cb1e63
2303031087063@paruluniversity.ac.in <2303031087063@paruluniversity.ac.in>
2025-02-25 16:38:21 +0530

    2303031087063

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./auth-user/login/login.component";
+import { RegisterComponent } from "./auth-user/register/register.component";
+import { ForgetpasswordComponent } from "./auth-user/forgetpassword/forgetpassword.component";
+import { UserComponent } from './auth-user/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [LoginComponent, RegisterComponent, ForgetpasswordComponent,UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Assignment1';
