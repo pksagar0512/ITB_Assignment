dad24b5d744b7064b918022b430789a3f395e64b
Manav Parmar <2303031087079@paruluniversity.ac.in>
2025-02-26 18:47:26 +0530

    2303031087079_Assignment1

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './manav/login/login.component';
+import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+import { ForgetPasswordComponent } from './auth-user/forget-password/forget-password.component';
+import { UserComponent } from './auth-user/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [LoginComponent, RegisterComponent, ForgetPasswordComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Assignment1';
 }
+
+
+
--- Assignment1/src/app/auth-user/auth-user.component.spec.ts
+++ Assignment1/src/app/auth-user/auth-user.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ForgetpasswordComponent } from './forgetpassword.component';
+import { AuthUserComponent } from './auth-user.component';
 
-describe('ForgetpasswordComponent', () => {
-  let component: ForgetpasswordComponent;
-  let fixture: ComponentFixture<ForgetpasswordComponent>;
+describe('AuthUserComponent', () => {
+  let component: AuthUserComponent;
+  let fixture: ComponentFixture<AuthUserComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ForgetpasswordComponent]
+      imports: [AuthUserComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ForgetpasswordComponent);
+    fixture = TestBed.createComponent(AuthUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
