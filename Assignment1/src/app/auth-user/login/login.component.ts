a405c6e8f90376532cc6e88f3241e865d07eba62
Ram Acharya <2303031087013@paruluniversity.ac.in>
2025-02-26 15:41:39 +0530

    Added assignment 1 changes

---
+++ Assignment1/src/app/auth-user/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    alert('Login successful (for demo purposes)!');
+  }
+}
