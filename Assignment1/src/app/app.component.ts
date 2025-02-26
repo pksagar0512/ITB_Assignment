b7ef6ef7d12bfc627d578b55f304bc8cd98d09ef
Shifan03 <pathanshifan773@gmail.com>
2025-02-26 21:47:59 +0530

    Added assignment 1 changes

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './shiff/login/login.component';
+import { RegisterComponent } from './shiff/register/register.component';
+import { ForgotpasswordComponent } from './shiff/forgotpassword/forgotpassword.component';
+import { UserComponent } from './shiff/user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent, RegisterComponent, ForgotpasswordComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
